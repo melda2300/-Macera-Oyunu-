6e6004b0e07a418dbcd66b66ce49e061b43f87f8
melda2300 <meldagayna21@gmail.com>
2024-08-06 23:28:55 +0300

    hatalar var düzeltilmeli

--- Macera-Oyunu/src/Location.java
+++ Macera-Oyunu/src/Location.java
@@ -1,2 +1,31 @@
-public class Location {
+public abstract class Location {
+    public String name;
+    private Player player;
+
+    public Location(String name, Player player) {
+        this.name = name;
+        this.player = player;
+    }
+
+
+    public abstract  boolean getLocantion();
+
+    abstract boolean onLocantion();
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 }
